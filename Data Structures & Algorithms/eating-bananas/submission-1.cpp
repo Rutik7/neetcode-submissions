4f35bf78c2f1768bbf5991b86f50324a25670c85
Rutik7 <88975212+Rutik7@users.noreply.github.com>
2026-08-03 06:14:04 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool helper(const vector<int>& piles, int h, int k)
+    {
+        int totalhours = 0;
+        //calculate the total hours needed for the current speed
+        for(int pile: piles)
+        {
+            //workaround for ceil(pile/k)
+            totalhours += (pile+k-1)/k;
+        }
+
+        return totalhours <= h;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        int ans;
+        int maxspeed = piles[0];
+        int minspeed = 1;
+        // If Koko eats at the size of the largest pile,
+        // she can finish every pile in at most one hour.
+        for(int i = 1;i<n;i++)
+        {
+            maxspeed = max(maxspeed,piles[i]);
+        }
+
+
+        // Binary search on the answer:
+        // speed lies between 1 and maxspeed.
+        while(minspeed <= maxspeed)
+        {
+            int mid = minspeed + (maxspeed-minspeed)/2;
+
+            if(helper(piles,h,mid))
+            {
+                ans = mid;
+                maxspeed = mid - 1;
+                // current speed works ... try to find a smaller vaild speed.
+                // check for minimum hours
+            }
+            else
+            {
+                minspeed = mid + 1;
+                // current speed is too slow
+                // Increase the eating speed.
+            }
+        }
+
+        
+        return ans;
+    }
+};
